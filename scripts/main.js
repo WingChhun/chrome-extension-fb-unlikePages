065d7f0b0ffacec40e54f46f289139d093a67182
James Chhun <capt.wing.chhun@gmail.com>
2018-12-02 10:33:51 -0800

    Organize main.js to be readable following OOP class organization

--- scripts/main.js
+++ scripts/main.js
@@ -18,18 +18,54 @@
 
 */
 
-//* Message to tell me where if this has loaded in the console
-console.log("Hello world main.js");
-
-//* Debug inspect what is possible in the document
-console.log("document", document);
-
-//* Inspect what is workable in the window
-console.log("window", window);
-
 //? Console.log( window.document URL get the full URL
 
 const {URL, baseURL} = window.document;
 
 //* Debug show the URLs
 console.log('URL', URL);
+
+//TODO: Add a on Document Load listener
+
+
+
+/*
+TODO(s)
+
+ * Need a way to pattern check what page currently on
+ * Make sure storing data works correctly
+ * Clicking the button should fire off an event to start the program
+
+*/
+class DriverProgram
+{
+    constructor()
+    {}
+
+    start() {
+
+        const {URL, baseURL} = window.document;
+
+        //* debug message
+        console.log('driver.start()');
+
+        console.log("window", window);
+
+        console.log('URL', URL);
+
+
+    }
+
+
+
+
+
+}(function mainProgram() {
+
+    const driver = new DriverProgram();
+
+    console.log("mainProgram loaded");
+
+    document.addEventListener("DOMContentLoaded", driver.start);
+
+})();
